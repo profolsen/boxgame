35d18a3e01d68a5802eb7aa6d7171c59c9310192
paul <olsenp@strose.edu>
2021-03-31 16:07:10 -0400

    fixing so parameters are correct.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Board b = new Board(100);
-        for(int i = 0; i < 10; i++) {
+        Board b = new Board(9);
+        for(int i = 0; i < 4; i++) {
             new Player(b);
         }
         b.startGame();
